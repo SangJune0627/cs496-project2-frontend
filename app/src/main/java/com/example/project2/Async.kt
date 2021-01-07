c280fa1a98d84e86377b3480238c80fbb67aab34
Park Sang June <67790478+SangJune0627@users.noreply.github.com>
2021-01-07 22:06:30 +0900

    http is working

--- app/src/main/java/com/example/project2/Async.kt
+++ app/src/main/java/com/example/project2/Async.kt
@@ -1,6 +1,7 @@
 package com.example.project2
 
 import android.os.AsyncTask
+import android.util.Log
 import java.io.IOException
 import java.net.HttpURLConnection
 import java.net.MalformedURLException
@@ -9,14 +10,14 @@ import java.net.URL
 class Async : AsyncTask<Any?, Any?, Any?>() {
     override fun doInBackground(objects: Array<Any?>): Any? {
         try {
-            var url: URL = URL("http://google.co.kr")
-            var http: HttpURLConnection = url.openConnection() as HttpURLConnection
+            val url: URL = URL("http://192.249.18.171:3000")
+            val http: HttpURLConnection = url.openConnection() as HttpURLConnection
             println(http.responseCode)
-//            println("응답메세지: " + http.responseMessage)
+            println(http.responseMessage)
         }catch (e: MalformedURLException) {
             println("http://127.0.0.1:3000 is not a URL I understand");
         }catch (e: IOException) {
-            println("응답2")
+            println("응답2"+e.message)
         }
         return null
     }
