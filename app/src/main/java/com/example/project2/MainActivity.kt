cdbb92440fbc4c7ee84480278736f59f0b9629d8
Park Sang June <67790478+SangJune0627@users.noreply.github.com>
2021-01-07 20:49:00 +0900

    Http is not working

--- app/src/main/java/com/example/project2/MainActivity.kt
+++ app/src/main/java/com/example/project2/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.project2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.io.IOException
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var async:Async=Async()
+        async.execute()
     }
 }
