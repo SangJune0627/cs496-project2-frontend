24f09bd2abb5c23053fe46b55fd73bc14e2de6b7
Park Sang June <67790478+SangJune0627@users.noreply.github.com>
2021-01-07 17:15:56 +0900

    Initial commit(app)

---
+++ app/src/main/java/com/example/project2/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.project2
+
+import android.app.DownloadManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.textclassifier.TextLinks
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
