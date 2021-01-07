81f75b48f65c19108ab825323fa32363513e5841
Park Sang June <67790478+SangJune0627@users.noreply.github.com>
2021-01-07 17:44:50 +0900

    Initial commit

--- app/src/main/java/com/example/project2/MainActivity.kt
+++ app/src/main/java/com/example/project2/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.example.project2
 
-import android.app.DownloadManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.textclassifier.TextLinks
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
