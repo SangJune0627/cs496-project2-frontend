cf74759e59f8ea38c8b4e4f73cf91217c64f60cc
p51lee <p51lee@kaist.ac.kr>
2021-01-09 15:40:37 +0900

    Add facebook tab

--- app/src/main/java/com/example/project2/Async.kt
+++ app/src/main/java/com/example/project2/Async.kt
@@ -2,27 +2,136 @@ package com.example.project2
 
 import android.os.AsyncTask
 import android.util.Log
-import java.io.BufferedReader
-import java.io.IOException
-import java.io.InputStreamReader
-import java.net.HttpURLConnection
-import java.net.MalformedURLException
-import java.net.URL
+import com.google.gson.annotations.SerializedName
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.*
 
 class Async : AsyncTask<Any?, Any?, Any?>() {
     override fun doInBackground(objects: Array<Any?>): Any? {
-        try {
-            val url: URL = URL("http://192.249.18.171:3000")
-            val http: HttpURLConnection = url.openConnection() as HttpURLConnection
-            val br = BufferedReader(InputStreamReader(http.inputStream))
-            println(http.responseCode)
-            println(http.responseMessage)
-            println(br.readLine())
-        }catch (e: MalformedURLException) {
-            println("http://127.0.0.1:3000 is not a URL I understand");
-        }catch (e: IOException) {
-            println("응답2"+e.message)
-        }
+//        try {
+//            val url: URL = URL("http://192.249.18.171:3000")
+//            val http: HttpURLConnection = url.openConnection() as HttpURLConnection
+//            val br = BufferedReader(InputStreamReader(http.inputStream))
+//            println(http.responseCode)
+//            println(http.responseMessage)
+//            println(br.readLine())
+//        }catch (e: MalformedURLException) {
+//            println("http://127.0.0.1:3000 is not a URL I understand");
+//        }catch (e: IOException) {
+//            println("응답2"+e.message)
+//        }
+
+//        try {
+//            val url2: URL = URL("http://192.249.18.171:3000")
+//            val http2: HttpURLConnection = url2.openConnection() as HttpURLConnection
+//
+//            http2.defaultUseCaches = false
+//            http2.doInput = true
+//            http2.doOutput = true
+//            http2.requestMethod = "POST"
+//
+//            var buffer = StringBuffer()
+//            buffer.append("yay")
+//
+//            val outStream = OutputStreamWriter(http2.outputStream, "EUC-KR")
+//            val writer = PrintWriter(outStream)
+//            writer.write(buffer.toString())
+//            writer.flush()
+//            Log.d("yay", "finish")
+//            println(http2.requestMethod)
+//        } catch(e: MalformedURLException) {
+//
+//        } catch(e: IOException) {
+//
+//        }
+
+        var retrofit = Retrofit.Builder().baseUrl("http://192.249.18.171:3000")
+            .addConverterFactory(ScalarsConverterFactory.create()).build()
+
+        var retrofit2 = Retrofit.Builder().baseUrl("http://192.249.18.171:3000")
+            .addConverterFactory(GsonConverterFactory.create()).build()
+
+        var service1 = retrofit.create(RetrofitService::class.java)
+        var service2 = retrofit2.create(RetrofitService2::class.java)
+
+        var call1 = service1.getPosts("yay")
+        var call2 = service2.getPosts("1")
+
+        call1.enqueue(object: Callback<String> {
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                if (response.isSuccessful) {
+                    val result = response.body()
+                    Log.d("response", "onResponse: 성공, \n" + result.toString())
+                } else {
+                    Log.d("respose", "onResponse: 실패")
+                }
+            }
+
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                Log.d("통신실패", "onFailure: " + t.message)
+            }
+        })
+//
+//        Thread.sleep(1000)
+//        call2.enqueue(object: Callback<PostResult> {
+//            override fun onResponse(call: Call<PostResult>, response: Response<PostResult>) {
+//                if (response.isSuccessful) {
+//                    val result = response.body()
+//                    Log.d("response", "onResponse: 성공, \n" + result.toString())
+//                } else {
+//                    Log.d("respose", "onResponse: 실패")
+//                }
+//            }
+//
+//            override fun onFailure(call: Call<PostResult>, t: Throwable) {
+//                Log.d("통신실패asdfasdf", "onFailure: " + t.message)
+//            }
+//        })
+
         return null
     }
+
+}
+
+class PostResult {
+//    @SerializedName으로 일일히 안해주면 아예 이름이 같아야함
+
+    @SerializedName("userId")
+    private var userId: Int = 0
+
+    @SerializedName("id")
+    private var id: Int = 0
+
+    @SerializedName("title")
+    private lateinit var title: String
+
+    @SerializedName("body")
+    private lateinit var body: String
+
+    override fun toString(): String {
+        return "PostResult{" +
+                "userId=" + userId +
+                ", id=" + id +
+                ", title='" + title + '\'' +
+                ", bodyValue='" + body + '\'' +
+                '}'
+    }
+
+}
+
+
+
+interface RetrofitService {
+    @Headers("Content-Type: application/json")
+    @POST("test")
+    fun getPosts(@Body body: String): Call<String>
+}
+interface RetrofitService2 {
+    @GET("posts/{post}")
+    fun getPosts(@Path("post") post: String): Call<PostResult>
 }
--- app/src/main/java/com/example/project2/FourthFragment.kt
+++ app/src/main/java/com/example/project2/FourthFragment.kt
@@ -1,37 +1,19 @@
 package com.example.project2
 
-import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
-import android.content.ServiceConnection
-import android.content.pm.PackageManager
-import android.media.tv.TvContract.Programs.Genres.encode
-import android.os.Build
 import android.os.Bundle
-import android.os.IBinder
 import android.util.Log
-import android.util.TypedValue
-import android.view.Gravity
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.LinearLayout
-import android.widget.TextView
-import androidx.annotation.RequiresApi
-import androidx.core.view.setPadding
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
-import com.example.project2.Stopwatch.StopwatchService
 import com.facebook.*
-import com.facebook.login.LoginManager
 import com.facebook.login.LoginResult
 import com.facebook.login.widget.LoginButton
 import org.json.JSONException
 import org.json.JSONObject
-import java.net.URLEncoder.encode
-import java.security.MessageDigest
-import java.util.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -40,15 +22,16 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [ThirdFragment.newInstance] factory method to
+ * Use the [FourthFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ThirdFragment : Fragment() {
+class FourthFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
     private lateinit var myContext: FragmentActivity
+    private lateinit var viewOfLayout: View
 
     private lateinit var callbackManager: CallbackManager
 
@@ -62,16 +45,12 @@ class ThirdFragment : Fragment() {
         myContext = context as FragmentActivity
     }
 
-    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
-//        LoginManager.getInstance().
-
-
     }
 
     override fun onCreateView(
@@ -79,7 +58,8 @@ class ThirdFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val viewOfLayout = inflater.inflate(R.layout.fragment_third, container, false)
+        viewOfLayout = inflater.inflate(R.layout.fragment_fourth, container, false)
+
         FacebookSdk.sdkInitialize(myContext)
 
         callbackManager = CallbackManager.Factory.create()
@@ -88,7 +68,7 @@ class ThirdFragment : Fragment() {
         loginButton.setPermissions(listOf("email"))
         loginButton.setFragment(this)
         Log.d("fick", "fock")
-        loginButton.registerCallback(callbackManager, object: FacebookCallback<LoginResult>{
+        loginButton.registerCallback(callbackManager, object: FacebookCallback<LoginResult> {
             override fun onSuccess(result: LoginResult?) {
                 if(result?.accessToken != null) {
                     val accessToken = result.accessToken
@@ -104,13 +84,6 @@ class ThirdFragment : Fragment() {
                 Log.e("LoginErr", error.toString())
             }
         })
-
-
-//        var async = Async()
-//        async.execute()
-
-
-
         return viewOfLayout
     }
 
@@ -139,7 +112,6 @@ class ThirdFragment : Fragment() {
         graphRequest.executeAsync()
     }
 
-
     companion object {
         /**
          * Use this factory method to create a new instance of
@@ -147,12 +119,12 @@ class ThirdFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment ThirdFragment.
+         * @return A new instance of fragment FourthFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            ThirdFragment().apply {
+            FourthFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
